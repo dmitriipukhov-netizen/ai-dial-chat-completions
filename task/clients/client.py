95710d61a45b27c76f8b0c6d0c69a31f2edbd8a5
Dmitrii Pukhov <dmitrii_pukhov@epam.com>
2025-12-07 23:22:59 +0400

    Implement DIAL AI clients

--- task/clients/client.py
+++ task/clients/client.py
@@ -1,4 +1,7 @@
+from typing import cast
+
 from aidial_client import Dial, AsyncDial
+from aidial_client.types.chat import ChatCompletionResponse, Message as RequestMessage
 
 from task.clients.base import BaseClient
 from task.constants import DIAL_ENDPOINT
@@ -10,26 +13,47 @@ class DialClient(BaseClient):
 
     def __init__(self, deployment_name: str):
         super().__init__(deployment_name)
-        #TODO:
-        # Documentation: https://pypi.org/project/aidial-client/ (here you can find how to create and use these clients)
-        # 1. Create Dial client
-        # 2. Create AsyncDial client
+        self._client = Dial(base_url=DIAL_ENDPOINT, api_key=self._api_key)
+        self._async_client = AsyncDial(base_url=DIAL_ENDPOINT, api_key=self._api_key)
 
     def get_completion(self, messages: list[Message]) -> Message:
-        #TODO:
-        # 1. Create chat completions with client
-        #    Hint: to unpack messages you can use the `to_dict()` method from Message object
-        # 2. Get content from response, print it and return message with assistant role and content
-        # 3. If choices are not present then raise Exception("No choices in response found")
-        raise NotImplementedError
+        completion: ChatCompletionResponse = self._client.chat.completions.create(
+            deployment_name=self._deployment_name,
+            messages=self._prepare_request_messages(messages),
+        )
+        choices = completion.choices
+        if not choices:
+            raise Exception(f"No choices in response found")
+
+        message = Message(Role.AI, choices[0].message.content)
+        print(message.content)
+        return message
 
     async def stream_completion(self, messages: list[Message]) -> Message:
-        #TODO:
-        # 1. Create chat completions with async client
-        #    Hint: don't forget to add `stream=True` in call.
-        # 2. Create array with `contents` name (here we will collect all content chunks)
-        # 3. Make async loop from `chunks` (from 1st step)
-        # 4. Print content chunk and collect it contents array
-        # 5. Print empty row `print()` (it will represent the end of streaming and in console we will print input from a new line)
-        # 6. Return Message with assistant role and message collected content
-        raise NotImplementedError
+        completion = await self._async_client.chat.completions.create(
+            deployment_name=self._deployment_name,
+            messages=self._prepare_request_messages(messages),
+            stream=True
+        )
+
+        contents = []
+        async for chunk in completion:
+            choices = chunk.choices
+            if not choices:
+                raise Exception(f"No choices in response chunk found")
+            choice = choices[0]
+            if choice.finish_reason:
+                print()
+                break
+            content = choice.delta.content
+            print(content, end='')
+            contents.append(content)
+
+        print()
+
+        return Message(Role.AI, ''.join(contents))
+
+    @staticmethod
+    def _prepare_request_messages(messages: list[Message]) -> list[RequestMessage]:
+        # The return type of task.models.Message.to_dict() is too wide type, "dirty" typecast is needed
+        return [cast(RequestMessage, cast(object, m.to_dict())) for m in messages]
--- task/clients/custom_client.py
+++ task/clients/custom_client.py
@@ -1,6 +1,8 @@
 import json
 import aiohttp
 import requests
+from aiohttp import ClientResponseError
+from requests import HTTPError
 
 from task.clients.base import BaseClient
 from task.constants import DIAL_ENDPOINT
@@ -8,7 +10,7 @@ from task.models.message import Message
 from task.models.role import Role
 
 
-class DialClient:
+class DialClient(BaseClient):
     _endpoint: str
     _api_key: str
 
@@ -17,34 +19,89 @@ class DialClient:
         self._endpoint = DIAL_ENDPOINT + f"/openai/deployments/{deployment_name}/chat/completions"
 
     def get_completion(self, messages: list[Message]) -> Message:
-        #TODO:
-        # Take a look at README.md of how the request and regular response are looks like!
-        # 1. Create headers dict with api-key and Content-Type
-        # 2. Create request_data dictionary with:
-        #   - "messages": convert messages list to dict format using msg.to_dict() for each message
-        # 3. Make POST request using requests.post() with:
-        #   - URL: self._endpoint
-        #   - headers: headers from step 1
-        #   - json: request_data from step 2
-        # 4. Get content from response, print it and return message with assistant role and content
-        # 5. If status code != 200 then raise Exception with format: f"HTTP {response.status_code}: {response.text}"
-        raise NotImplementedError
+        request_data = {
+            'model': self._deployment_name,
+            'messages': self._prepare_request_messages(messages),
+        }
+
+        response = requests.post(self._endpoint, headers=self._headers, json=request_data)
+        try:
+            response.raise_for_status()
+        except HTTPError as e:
+            raise Exception(f"HTTP {response.status_code}: {response.text}") from e
+
+        resp_data = response.json()
+        if not (raw_choices := resp_data.get('choices', [])):
+            raise Exception(f"No choices in response found")
+        if not (raw_message := raw_choices[0].get('message')):
+            raise Exception(f"No message in response found")
+        if (raw_role := raw_message.get('role')) not in Role:
+            raise Exception(f"Wrong role '{raw_role}'")
+        if not (raw_content := raw_message.get('content')):
+            raise Exception(f"No content in response found")
+
+        message = Message(Role(raw_role), raw_content)
+        print(message.content)
+
+        return message
 
     async def stream_completion(self, messages: list[Message]) -> Message:
-        #TODO:
-        # Take a look at README.md of how the request and streamed response chunks are looks like!
-        # 1. Create headers dict with api-key and Content-Type
-        # 2. Create request_data dictionary with:
-        #    - "stream": True  (enable streaming)
-        #    - "messages": convert messages list to dict format using msg.to_dict() for each message
-        # 3. Create empty list called 'contents' to store content snippets
-        # 4. Create aiohttp.ClientSession() using 'async with' context manager
-        # 5. Inside session, make POST request using session.post() with:
-        #    - URL: self._endpoint
-        #    - json: request_data from step 2
-        #    - headers: headers from step 1
-        #    - Use 'async with' context manager for response
-        # 6. Get content from chunks (don't forget that chunk start with `data: `, final chunk is `data: [DONE]`), print
-        #    chunks, collect them and return as assistant message
-        raise NotImplementedError
+        request_data = {
+            'model': self._deployment_name,
+            'messages': self._prepare_request_messages(messages),
+            'stream': True,
+        }
 
+        contents = []
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(self._endpoint, headers=self._headers, json=request_data) as response:
+                try:
+                    response.raise_for_status()
+                except ClientResponseError as e:
+                    raise Exception(f"HTTP {response.status}: {response.text}") from e
+
+                resp_chunk = self._parse_stream_chunk(await response.content.readuntil(separator=b'\n\n'))
+                raw_chunk = json.loads(resp_chunk)
+                if not (raw_choices := raw_chunk.get('choices', [])):
+                    raise Exception(f"No choices in response found")
+                if (raw_role := raw_choices[0].get('delta', {}).get('role')) not in Role:
+                    raise Exception(f"Wrong role '{raw_role}'")
+                role = Role(raw_role)
+
+                while resp_chunk_bytes := await response.content.readuntil(separator=b'\n\n'):
+                    resp_chunk = self._parse_stream_chunk(resp_chunk_bytes)
+                    if resp_chunk == '[DONE]':
+                        break
+                    raw_chunk = json.loads(resp_chunk)
+                    if not (raw_choices := raw_chunk.get('choices', [])):
+                        raise Exception("No choices in response found")
+                    raw_choice = raw_choices[0]
+                    if raw_choice.get('finish_reason') is not None:
+                        break
+                    if not (raw_delta := raw_choice.get('delta')):
+                        print(raw_choices)
+                        raise Exception("No delta in response found")
+                    if not (raw_content := raw_delta.get('content')):
+                        raise Exception("No content in response found")
+
+                    print(raw_content, end='')
+                    contents.append(raw_content)
+        print('\n')
+
+        return Message(role, ''.join(contents))
+
+    @staticmethod
+    def _prepare_request_messages(messages: list[Message]) -> list[dict[str, str]]:
+        return [m.to_dict() for m in messages]
+
+    @property
+    def _headers(self) -> dict[str, str]:
+        return {
+            'Content-Type': 'application/json',
+            'API-KEY': self._api_key,
+        }
+
+    @staticmethod
+    def _parse_stream_chunk(chunk: bytes) -> str:
+        return chunk.decode('utf-8').strip().removeprefix('data: ')
